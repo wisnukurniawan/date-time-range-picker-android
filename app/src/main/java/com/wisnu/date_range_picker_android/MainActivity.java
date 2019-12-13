e46dff0920e13b751de8c9f650f1028ed11e2025
wisnu kurniawan <wisnuko13@gmail.com>
2019-12-14 06:44:51 +0700

    Update example

--- app/src/main/java/com/wisnu/date_range_picker_android/MainActivity.java
+++ app/src/main/java/com/wisnu/date_range_picker_android/MainActivity.java
@@ -2,23 +2,76 @@ package com.wisnu.date_range_picker_android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import com.wisnu.datetimerangepickerandroid.CalendarPickerView;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    Date selectedDate = new Date();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CalendarPickerView cal = findViewById(R.id.calendar_view);
+        final CalendarPickerView cal = findViewById(R.id.calendar_view);
         cal.init(
-            DateTime.now(DateTimeZone.UTC).minusYears(1).toDate(),
-            DateTime.now(DateTimeZone.UTC).plusDays(4).toDate()
+                DateTime.now(DateTimeZone.UTC).toDate(),
+                DateTime.now(DateTimeZone.UTC).plusDays(10).toDate()
         )
-            .inMode(CalendarPickerView.SelectionMode.RANGE)
-            .withSelectedDate(DateTime.now(DateTimeZone.UTC).minusDays(1).plusDays(4).toDate());
+                .inMode(CalendarPickerView.SelectionMode.RANGE)
+                .withSelectedDate(selectedDate);
+
+        final Button button1 = findViewById(R.id.button1);
+
+        button1.setText(selectedDate.toString());
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cal.init(
+                        DateTime.now(DateTimeZone.UTC).toDate(),
+                        DateTime.now(DateTimeZone.UTC).plusDays(10).toDate()
+                )
+                        .inMode(CalendarPickerView.SelectionMode.RANGE)
+                        .withSelectedDate(selectedDate);
+
+                cal.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+                    @Override
+                    public void onDateSelected(Date date) {
+                        Log.d("onDateSelected", date.toString());
+                        selectedDate = date;
+                        button1.setText(selectedDate.toString());
+                    }
+
+                    @Override
+                    public void onDateUnselected(Date date) {
+
+                    }
+                });
+            }
+        });
+
+        cal.setOnDateResolvedListener(new CalendarPickerView.OnDateResolvedListener() {
+            @Override
+            public void onMinDateResolved(Date date) {
+                selectedDate = date;
+                button1.setText(selectedDate.toString());
+                Log.d("onMinDateResolved", date.toString());
+            }
+
+            @Override
+            public void onMaxDateResolved(Date date) {
+                selectedDate = date;
+                button1.setText(selectedDate.toString());
+                Log.d("onMaxDateResolved", date.toString());
+            }
+        });
     }
+
 }
