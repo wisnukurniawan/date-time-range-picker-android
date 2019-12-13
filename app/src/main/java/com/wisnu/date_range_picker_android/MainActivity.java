745df4b2a1cb8eda9e818b8920ea3a6b0c862259
wisnu kurniawan <wisnuko13@gmail.com>
2019-12-13 23:43:27 +0700

    Update example

--- app/src/main/java/com/wisnu/date_range_picker_android/MainActivity.java
+++ app/src/main/java/com/wisnu/date_range_picker_android/MainActivity.java
@@ -2,9 +2,11 @@ package com.wisnu.date_range_picker_android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import com.wisnu.datetimerangepickerandroid.CalendarPickerView;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
         CalendarPickerView cal = findViewById(R.id.calendar_view);
         cal.init(
-            DateTime.now(DateTimeZone.UTC).minusYears(1).toDate(),
-            DateTime.now(DateTimeZone.UTC).plusDays(4).toDate()
+                DateTime.now(DateTimeZone.UTC).minusYears(1).toDate(),
+                DateTime.now(DateTimeZone.UTC).plusDays(1).toDate()
         )
-            .inMode(CalendarPickerView.SelectionMode.RANGE)
-            .withSelectedDate(DateTime.now(DateTimeZone.UTC).minusDays(1).plusDays(4).toDate());
+                .inMode(CalendarPickerView.SelectionMode.RANGE)
+                .withSelectedDate(DateTime.now(DateTimeZone.UTC).minusDays(1).toDate());
+        cal.setOnDateResolvedListener(new CalendarPickerView.OnDateResolvedListener() {
+            @Override
+            public void onMinDateResolved(Date date) {
+                Log.d("onMinDateResolved", date.toString());
+            }
+
+            @Override
+            public void onMaxDateResolved(Date date) {
+                Log.d("onMaxDateResolved", date.toString());
+            }
+        });
     }
 }
